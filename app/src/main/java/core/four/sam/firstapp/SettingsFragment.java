bed7ecbdc596eabe0b99987888246695cca7166f
sammyers <sam@students.olin.edu>
2016-09-15 20:38:40 -0400

    initial commit

---
+++ app/src/main/java/core/four/sam/firstapp/SettingsFragment.java
@@ -0,0 +1,19 @@
+package core.four.sam.firstapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by smyers on 9/15/2016.
+ */
+public class SettingsFragment extends Fragment {
+    public SettingsFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+    }
+}
---
+++ app/src/main/java/core/four/sam/firstapp/TodoListFragment.java
@@ -0,0 +1,21 @@
+package core.four.sam.firstapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TodoListFragment extends Fragment {
+
+    public TodoListFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
